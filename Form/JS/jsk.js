bb0a8a65505ea86b12e38c51256a2b2aa8465734
Oleksandra Matviienko <oleksandramt@Oleksandras-MacBook-Pro.home>
2021-03-05 21:20:56 +0100

    js tasks

--- Form/JS/jsk.js
+++ Form/JS/jsk.js
@@ -6,11 +6,27 @@
 //    alert('document')
 // })
 
-const form = document.getElementById('test-form');
+// const form = document.getElementById('test-form');
+const genderInputs = document.querySelectorAll('.j-gender-input');
 
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    event.stopPropagation();
+for (let input of genderInputs) {
+    input.addEventListener('change', function(e) {
+        const inputValue = e.target.value;
 
-    console.log(event);
-})
+        const textNodes = document.querySelectorAll('.j-text-gender');
+
+        for (let textNode of textNodes) {
+            textNode.classList.add('d-none');
+        }
+
+        document.querySelector(`.j-text-${inputValue}`).classList.remove('d-none');
+    })
+}
+
+
+// form.addEventListener('submit', function(event) {
+//     event.preventDefault();
+//     event.stopPropagation();
+
+//     console.log(event);
+// })
